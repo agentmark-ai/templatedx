06e41de7b182b541b95bdc1bef3c460e457a200e
Ryan Randall <ryan@puzzlet.ai>
2024-10-28 10:37:53 -0400

    Comments

--- src/scope.ts
+++ src/scope.ts
@@ -7,7 +7,6 @@ export class Scope {
     this.parent = parent;
   }
 
-  // Retrieves a variable, searching the current scope and parents
   get(key: string): any {
     if (key in this.variables) {
       return this.variables[key];
@@ -18,17 +17,14 @@ export class Scope {
     }
   }
 
-  // Retrieves a variable only from the current scope
   getLocal(key: string): any {
     return this.variables[key];
   }
 
-  // Sets a variable in the current scope
   setLocal(key: string, value: any): void {
     this.variables[key] = value;
   }
 
-  // Creates a new child scope
   createChild(variables: Record<string, any> = {}): Scope {
     return new Scope(variables, this);
   }
