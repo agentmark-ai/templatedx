501e5753c29e9a4ba44feabfe98b095f8898544b
Ryan Randall <ryan@puzzlet.ai>
2024-11-01 19:53:57 -0400

    Fix: import

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ import { ElementPluginRegistry } from "./element-plugin-registry";
 import { ElementPlugin, PluginContext } from "./element-plugin";
 import { FilterRegistry } from "./filter-registry";
 import type { FilterFunction } from "./filter-registry";
-import type { ExtractedField } from "./extract-fields";
 import type { ContentLoader } from "./types";
 import type { Root } from "mdast";
 import './register-builtin-plugins';
@@ -18,7 +17,6 @@ import './register-builtin-plugins';
 export type {
   ContentLoader,
   Root as Ast,
-  ExtractedField,
   PluginContext,
   FilterFunction,
 };
