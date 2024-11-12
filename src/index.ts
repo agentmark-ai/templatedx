0ce8961d09563d699686d2d70e5ac31e23ddd6eb
Ryan Randall <ryan@puzzlet.ai>
2024-11-12 10:22:19 -0500

    Fix: include global

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import type { FilterFunction } from "./filter-registry";
 import type { ContentLoader } from "./types";
 import type { Root } from "mdast";
 import type { BaseMDXProvidedComponents } from './types';
+import './global';
 import './register-builtin-plugins';
 
 export type {
